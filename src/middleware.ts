5b035e60f92b7f995817a4216bc546a6f151df33
antonio.trupac <sniper.trupac@gmail.com>
2023-03-23 22:48:48 +0100

    add clerk - client

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export default withClerkMiddleware((req: NextRequest) => {
+  console.log("RUNNING MIDDLEWARE");
+
+  return NextResponse.next();
+});
+
+// Stop Middleware running on static files
+export const config = {
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+};
