6e19f8884d8f5b8c508db6009581013af00f1ab3
antonio.trupac <sniper.trupac@gmail.com>
2023-03-24 21:32:17 +0100

    typography

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import { type AppType } from "next/app";
+import { Manrope } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
+const manrope = Manrope({
+  subsets: ["latin"],
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <main className={manrope.className}>
+        <Component {...pageProps} />
+      </main>
     </ClerkProvider>
   );
 };
