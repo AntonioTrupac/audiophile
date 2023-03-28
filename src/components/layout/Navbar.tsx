f2b3f5158e1bef73f7fa4a2082c4e4d6137437f9
antonio.trupac <sniper.trupac@gmail.com>
2023-03-28 18:15:26 +0200

    navbar fixes

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,38 +4,46 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <header className="fixed top-0 left-0 z-[9999] flex w-full justify-center">
+    <header className="absolute top-6 left-0 z-[9999] flex w-full flex-col items-center justify-center">
       <div className="flex w-full items-center justify-between lg:mx-12 lg:max-w-[1110px] xl:mx-0">
-        <div>
-          <Image
-            src="/assets/shared/desktop/logo.svg"
-            alt="logo"
-            width={143}
-            height={25}
-          />
-        </div>
+        <Image
+          src="/assets/shared/desktop/logo.svg"
+          alt="logo"
+          width={143}
+          height={25}
+        />
 
         <nav>
-          <ul className="flex">
+          <ul className="flex gap-[2.125rem]">
             <li>
-              <Link href="/">Home</Link>
+              <Link href="/" className="hover:text-primary">
+                Home
+              </Link>
             </li>
             <li>
-              <Link href="/headphones">Headphones</Link>
+              <Link href="/headphones" className="hover:text-primary">
+                Headphones
+              </Link>
             </li>
             <li>
-              <Link href="/speakers">Speakers</Link>
+              <Link href="/speakers" className="hover:text-primary">
+                Speakers
+              </Link>
             </li>
             <li>
-              <Link href="/earphones">Earphones</Link>
+              <Link href="/earphones" className="hover:text-primary">
+                Earphones
+              </Link>
             </li>
           </ul>
         </nav>
 
         <button onClick={() => console.log("Clicked")}>
-          <Cart />
+          <Cart className="text-white" />
         </button>
       </div>
+
+      <div className="mt-9 h-[1px] w-full bg-white/20 lg:max-w-[1110px]" />
     </header>
   );
 };
