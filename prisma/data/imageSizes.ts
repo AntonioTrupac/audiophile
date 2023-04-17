1ec56aec13d1bb10d4c257ceb52b86f075adcff2
antonio.trupac <sniper.trupac@gmail.com>
2023-04-17 20:46:32 +0200

    Added seeding data feature

--- prisma/data/imageSizes.ts
+++ prisma/data/imageSizes.ts
@@ -1,25 +1,34 @@
 export const imageSizes = [
   {
     id: 1,
-    mobileUrl:
+    mobileURL:
       "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681490051/audiophile/image-category-page-preview_mit1si.jpg",
-    tabletUrl:
+    tabletURL:
       "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681490055/audiophile/image-category-page-preview_aakqs7.jpg",
-    desktopUrl:
+    desktopURL:
       "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681490048/audiophile/image-category-page-preview_s3yc4b.jpg",
     categoryImageId: 1,
   },
   {
     id: 2,
-    mobileUrl: "",
-    tabletUrl: "",
-    desktopUrl: "",
+    mobileURL:
+      "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681472414/audiophile/image-category-page-preview_vnehoq.jpg",
+    tabletURL:
+      "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681472419/audiophile/image-category-page-preview_m0psql.jpg",
+    desktopURL:
+      "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681472402/audiophile/image-category-page-preview_fhmucs.jpg",
+
     categoryImageId: 2,
   },
   {
     id: 3,
-    mobileUrl: "",
-    tabletUrl: "",
-    desktopUrl: "",
+    mobileURL:
+      "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681472467/audiophile/image-category-page-preview_tx9vr6.jpg",
+    tabletURL:
+      "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681306116/audiophile/image-category-page-preview_cp6qof.jpg",
+    desktopURL:
+      "https://res.cloudinary.com/dg93gnfxf/image/upload/v1681472443/audiophile/image-category-page-preview_vpniwi.jpg",
+
+    categoryImageId: 3,
   },
 ];
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,45 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "../src/server/db";
+import { categoryImage } from "./data/categoryImage";
+import { imageSizes } from "./data/imageSizes";
+import { product } from "./data/product";
 
 async function main() {
   console.log("Start seeding ...");
+
+  await prisma.imageSize.deleteMany();
+  console.log("Deleted records in imageSize table");
+
+  await prisma.categoryImage.deleteMany();
+  console.log("Deleted records in categoryImage table");
+
+  await prisma.product.deleteMany();
+  console.log("Deleted records in product table");
+
+  await prisma.$queryRaw`ALTER TABLE Product AUTO_INCREMENT = 1`;
+  await prisma.$queryRaw`ALTER TABLE CategoryImage AUTO_INCREMENT = 1`;
+  await prisma.$queryRaw`ALTER TABLE ImageSize AUTO_INCREMENT = 1`;
+
+  const p = await prisma.product.createMany({
+    data: product,
+  });
+  console.log("Created records in product table");
+
+  const cI = await prisma.categoryImage.createMany({ data: categoryImage });
+  console.log("Created records in categoryImage table");
+
+  const iS = await prisma.imageSize.createMany({
+    data: imageSizes,
+  });
+
+  console.log("Seeding finished.");
 }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
