1de2b52290219b3a738a6968139768e440f522c6
antonio.trupac <sniper.trupac@gmail.com>
2023-05-15 19:25:25 +0200

    lint fix for vs code

--- src/components/sections/SuggestedProductsSection.tsx
+++ src/components/sections/SuggestedProductsSection.tsx
@@ -27,27 +27,29 @@ const SuggestedProductsSection = ({
     <div className="flex flex-col items-center">
       <h2 className="mb-8 text-2xl font-bold">You may also like</h2>
 
-      {suggestedProducts.map((suggestedProduct) => {
-        const product = suggestedProduct.suggestedProduct;
-        console.log(product);
-        return (
-          <div
-            key={suggestedProduct.id}
-            className="relative flex aspect-[1] w-full md:aspect-[0.585416667] md:min-h-[200px] lg:aspect-[0.964285714] lg:h-[560px] lg:w-[540px]"
-          >
-            {product.images?.imageSizes &&
-              product.images.imageSizes.map((image) => (
-                <Image
-                  key={image.id}
-                  src={image.desktopURL}
-                  alt={product.name}
-                  fill
-                  className="rounded-md md:hidden lg:flex"
-                />
-              ))}
-          </div>
-        );
-      })}
+      <div className="flex gap-[1.875rem]">
+        {suggestedProducts.map((suggestedProduct) => {
+          const product = suggestedProduct.suggestedProduct;
+          console.log(product);
+          return (
+            <div
+              key={suggestedProduct.id}
+              className="relative flex h-[318px] w-[350px]"
+            >
+              {product.images?.imageSizes &&
+                product.images.imageSizes.map((image) => (
+                  <Image
+                    key={image.id}
+                    src={image.mobileURL}
+                    alt={product.name}
+                    fill
+                    className="rounded-md md:hidden lg:flex"
+                  />
+                ))}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
